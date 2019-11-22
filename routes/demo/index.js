6d5db99afaf027bffa64db866e17af2c28d62af1
Max Chou <max.chou@tw.viewsonic.com>
2019-11-22 17:40:15 +0800

    - update koa-router

--- routes/demo/index.js
+++ routes/demo/index.js
@@ -1,4 +1,4 @@
-const router = require('koa-router')()
+const router = require('@koa/router')()
 
 const middlewares = require('../utils/middlewares')
 const controllers = require('./controllers')
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
-const router = require('koa-router')()
+const router = require('@koa/router')()
 
 router.prefix('/api/v1')
 
-router.all('/demo', require('./demo').routes())
+router.use('/demo', require('./demo').routes())
 
 module.exports = router
